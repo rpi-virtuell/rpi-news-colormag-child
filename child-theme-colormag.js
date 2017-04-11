b7f996fc8366f683794cce4ef8dcad96ef8c1cec
Joachim Happel <joachim.happel@gmail.com>
2017-04-11 15:38:37 +0200

    fix header

--- child-theme-colormag.js
+++ child-theme-colormag.js
@@ -1,4 +1,22 @@
 jQuery(document).ready(function(){
     jQuery('#header-text').insertBefore('#wp-custom-header a');
+
+    jQuery('#category-posts-8 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/category/aus-den-instituten/\'');
+    jQuery('#category-posts-8 h3').css('cursor','pointer');
+
+    jQuery('#category-posts-7 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/category/digitale-raeume/\'');
+    jQuery('#category-posts-7 h3').css('cursor','pointer');
+
+    jQuery('#category-posts-4 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/category/medien/\'');
+    jQuery('#category-posts-4 h3').css('cursor','pointer');
+
+    jQuery('#category-posts-9 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/category/news/\'');
+    jQuery('#category-posts-9 h3').css('cursor','pointer');
+
+    jQuery('#em_widget-5 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/veranstaltungen/\'');
+    jQuery('#em_widget-5 h3').css('cursor','pointer');
+
+    jQuery('#em_widget-3 h3').attr('onclick','location.href=\'http://news.rpi-virtuell.de/veranstaltungen/\'');
+    jQuery('#em_widget-3 h3').css('cursor','pointer');
 });
 
